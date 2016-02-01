d0c919db7656a017f5781184fc6afd03fd9d178f
mawiha <mwhaase@gmail.com>
2016-01-31 20:15:14 -0500

    Changed the username and password to be generic

--- TPN_CDRs.py
+++ TPN_CDRs.py
@@ -9,7 +9,7 @@ import json
 req = urllib2.Request("https://reports.thinkingphones.com/analytics/webservices/content/realtime/doQuery?path=voice/raw_data/realtime_call_detail_records.cda&dataAccessId=2&fromDate=2016-1-01&toDate=2016-01-14&pageSize=10&pageNumber=1&outputType=json");
 
 # Add in your ThinkingPhones credentials
-base64string = base64.encodestring('%s:%s' % ('mhaase.test', 'Think123')).replace('\n', '')
+base64string = base64.encodestring('%s:%s' % ('<your username>', '<password>')).replace('\n', '')
 
 req.add_header("Authorization", "Basic %s" % base64string)
 response = urllib2.urlopen(req).read()
